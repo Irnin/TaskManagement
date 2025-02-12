53092a7b0c7577f987f5264549a6a3f8199051b2
Łukasz Michalak <lupinummail@gmail.com>
2025-02-12 20:00:22 +0100

    Added board view and option to unassigne

--- models/BoardModel.py
+++ models/BoardModel.py
@@ -1,3 +1,17 @@
 class BoardModel:
 	def __init__(self, masterModel):
 		self.masterModel = masterModel
+
+	def fetch_assigned_tasks(self, page, page_size, id):
+
+		return self.masterModel.send_request(
+			endpoint=f"/api/tasks/user/{id}",
+			method="GET",
+			params={"page": page, "size": page_size}
+		)
+
+	def unassigne_task(self, id):
+		self.masterModel.send_request(
+			endpoint=f"/api/tasks/unassigne/{id}",
+			method="PATCH"
+		)
--- views/BoardView.py
+++ views/BoardView.py
@@ -1,8 +1,71 @@
 import tkinter as tk
 
+from views.modules.IconButton import IconButton
+from views.modules.PageFrame import PageFrame
 from views.modules.Panel import Panel
 
 class BoardView(Panel):
     def __init__(self, parent, controller):
         super().__init__(parent, "Board")
         self.controller = controller
+
+        self.selected_task = None
+
+        self._setup_ui()
+
+    def _setup_ui(self):
+        main_frame = tk.Frame(self)
+        self.load_main_page(main_frame)
+
+        main_frame.columnconfigure(0, weight=1)
+        main_frame.columnconfigure(1, weight=0)
+        main_frame.rowconfigure(0, weight=1)
+
+        data_frame = tk.Frame(main_frame, padx=10)
+        data_frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+        action_frame = tk.Frame(main_frame)
+        action_frame.grid(row=0, column=1, sticky=tk.NS)
+
+        # Configure the table
+        self.table = PageFrame(data_frame, self.controller.fetch_assigned_tasks, self.controller.open_task, self.task_selected, self.task_unselected)
+        self.table.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        columns = [{'column_name': 'idTask', 'column_label': 'ID', 'visible': False},
+                   {'column_name': 'title', 'column_label': 'Title', 'visible': True},
+                   {'column_name': 'description', 'column_label': 'Description', 'visible': False},
+                   {'column_name': 'taskScore', 'column_label': 'Score', 'visible': True},
+                   {'column_name': 'taskCreated', 'column_label': 'Created', 'visible': False},
+                   {'column_name': 'dueDate', 'column_label': 'Due date', 'visible': True},
+                   {'column_name': 'categoryName', 'column_label': 'Category', 'visible': True}]
+
+        self.table.configure_columns(columns)
+        self.table.load_data()
+
+        # Configure the buttons
+
+        self.assign_task_button = IconButton(action_frame, "assign.png", "Unassigne task", lambda: self.controller.unassigne_task(self.selected_task['idTask']))
+        self.assign_task_button.pack(side=tk.TOP)
+        self.assign_task_button.configure(state=tk.DISABLED)
+
+        if self.controller.masterModel.is_admin():
+            self.delete_task_button = IconButton(action_frame, "delete.png", "Delete task", lambda: self.controller.delete_task(self.selected_task['idTask']))
+            self.delete_task_button.pack(side=tk.BOTTOM)
+            self.delete_task_button.configure(state=tk.DISABLED)
+
+
+    def task_selected(self, task):
+        self.selected_task = task
+
+        self.assign_task_button.configure(state=tk.NORMAL)
+
+        if self.controller.masterModel.is_admin():
+            self.delete_task_button.configure(state=tk.NORMAL)
+
+    def task_unselected(self):
+        self.selected_task = None
+
+        self.assign_task_button.configure(state=tk.DISABLED)
+
+        if self.controller.masterModel.is_admin():
+            self.delete_task_button.configure(state=tk.DISABLED)
